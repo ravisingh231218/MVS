51294bb385e47a38e92a0690d41c45c8899861dc
Parvej Ali <parvej291710@gmail.com>
2023-09-02 17:08:30 +0530

    Add project files.

---
+++ MVS/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVS.Models;
+
+namespace MVS.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp(Users user)
+        {
+            if(ModelState.IsValid)
+            {
+
+            }
+            return View();
+        }
+    }
+}
---
+++ MVS/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿namespace MVS.Models
+{
+    public class Users
+    {
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+    }
+}
